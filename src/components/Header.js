8ee387604a64ae6b306408ed07ec49765865b36a
Marcos Anthony <130773144+marcosanthony7@users.noreply.github.com>
2024-12-05 11:51:08 -0300

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './assets/css/fontawesome.css';
+import './assets/css/templatemo-lugx-gaming.css';
+import './assets/css/owl.css';
+import './assets/css/animate.css';
+
