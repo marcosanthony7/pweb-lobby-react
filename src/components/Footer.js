da68bfbff65b893688c097104cd8a716f355a80e
Marcos Anthony <130773144+marcosanthony7@users.noreply.github.com>
2024-12-11 09:22:14 -0300

    Delete src/components/Footer.js

--- src/components/Footer.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './assets/css/fontawesome.css';
-import './assets/css/templatemo-lugx-gaming.css';
-import './assets/css/owl.css';
-import './assets/css/animate.css';
-
